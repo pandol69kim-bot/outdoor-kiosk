1f525515dc4c9fd94907ebb8771fb32284816cfd
pandol <pandol69.kim@gmail.com>
2026-06-01 17:57:02 +0900

    feat: Revamp AdminNav component with responsive menu toggle and improved layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,9 +2,9 @@ import { AdminNav } from '@/components/admin/AdminNav'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 lg:flex">
       <AdminNav />
-      <div className="flex-1 overflow-auto">
+      <div className="flex-1 overflow-x-hidden">
         {children}
       </div>
     </div>
